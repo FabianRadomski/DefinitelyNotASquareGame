ff7d5b8bf700081a6534e3c45e65186a17b65c48
Fabian Radomski <F.J.Radomski@student.tudelft.nl>
2020-08-04 01:44:47 +0200

    Add the "move" functionality for the square.

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import pygame
 import sys
 
+pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+box = pygame.Rect(10, 10, 70, 70)
+
 
 while True:
     # checks if user quits
@@ -9,6 +12,19 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit(0)
 
+    # check input
+    # keys is a dictionary of booleans
+    keys = pygame.key.get_pressed()
+    if keys [pygame.K_d]:
+        box.x += 1
+    if keys[pygame.K_a]:
+        box.x -= 1
+    if keys[pygame.K_w]:
+        box.y -= 1
+    if keys[pygame.K_s]:
+        box.y += 1
+
     # draws a rectangle
-    pygame.draw.rect(screen, (25, 25, 255), pygame.Rect(10, 50, 200, 100))
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (25, 25, 255), box)
     pygame.display.flip()
